45d736d69156ec175f1b78cb01fc1421590a700c
Steven Calderon <scalderon1504@gmail.com>
2022-06-30 11:26:52 -0500

    Se modifican las configuraciones para rabbit

--- src/main/java/co/com/sofka/cargame/infra/config/JuegoConfig.java
+++ src/main/java/co/com/sofka/cargame/infra/config/JuegoConfig.java
@@ -2,10 +2,6 @@ package co.com.sofka.cargame.infra.config;
 
 import co.com.sofka.business.generic.ServiceBuilder;
 import co.com.sofka.business.generic.UseCase;
-import co.com.sofka.cargame.SocketController;
-import co.com.sofka.cargame.infra.bus.EventListenerSubscriber;
-import co.com.sofka.cargame.infra.bus.EventSubscriber;
-import co.com.sofka.cargame.infra.bus.NATSEventSubscriber;
 import co.com.sofka.cargame.infra.services.CarrilCarroQueryService;
 import co.com.sofka.cargame.infra.services.CarroQueryService;
 import co.com.sofka.cargame.infra.services.JuegoQueryService;
@@ -14,28 +10,21 @@ import co.com.sofka.cargame.usecase.listeners.*;
 import co.com.sofka.infraestructure.asyn.SubscriberEvent;
 import co.com.sofka.infraestructure.bus.EventBus;
 import co.com.sofka.infraestructure.repository.EventStoreRepository;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
 import java.util.Set;
 
 @Configuration
 public class JuegoConfig {
+    public static final String EXCHANGE = "cargame";
 
     @Bean
     public SubscriberEvent subscriberEvent(EventStoreRepository eventStoreRepository, EventBus eventBus) {
         return new SubscriberEvent(eventStoreRepository, eventBus);
-    }
-
-    @Bean
-    public EventSubscriber eventSubscriber(@Value("${spring.nats.uri}") String uri, EventListenerSubscriber eventListenerSubscriber, SocketController socketController) throws IOException, InterruptedException {
-        var eventSubs = new NATSEventSubscriber(uri, eventListenerSubscriber, socketController);
-        eventSubs.subscribe("juego.>", "handles.juego");
-        eventSubs.subscribe("carro.>", "handles.carro");
-        eventSubs.subscribe("carril.>", "handles.carril");
-        return eventSubs;
     }
 
     @Bean
@@ -70,6 +59,13 @@ public class JuegoConfig {
                 new UseCase.UseCaseWrap("carro.KilometrajeCambiado", (UseCase) moverCarroEnCarrilUseCase),
                 new UseCase.UseCaseWrap("juego.JuegoFinalizado", (UseCase) notificarGanadoresUseCase)
         );
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(RabbitTemplate rabbitTemplate) {
+        var rabbitAdmin = new RabbitAdmin(rabbitTemplate);
+        rabbitAdmin.declareExchange(new TopicExchange(EXCHANGE));
+        return rabbitAdmin;
     }
 
 }
